e4ceb6f3623028def50f98054e50c05324d7bfc6
rubasider <rubashka12@gmail.com>
2020-06-04 14:26:48 +0300

    style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
 import Movies from './Components/Movies/Movies';
+import SearchBar from './Components/SearchBar/SearchBar';
 function App() {
   return (
     <div className="App">
+      <img
+        src="https://cdn3.iconfinder.com/data/icons/ui-03-basic-2/100/Basic__54-512.png"
+        className="icon"
+      />
+      <SearchBar />
       <Movies />
     </div>
   );
