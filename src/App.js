944d871b941ea00c111365e648c01b945cf7f5dd
Hanan795 <hawawdeh95@gmail.com>
2020-06-04 17:41:28 +0300

    style

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import Movies from './Components/Movies/Movies';
 function App() {
   return (
     <div className="App">
-      <img
-        src="https://cdn3.iconfinder.com/data/icons/ui-03-basic-2/100/Basic__54-512.png"
-        className="icon"
-      />
+      <header className = 'header'>
+        <img
+          alt = 'icon'
+          src="https://cdn3.iconfinder.com/data/icons/ui-03-basic-2/100/Basic__54-512.png"
+          className="icon"
+        />
+        <h3>Movies Pool</h3>
+      </header>
+      <div>
+        <p>Here you can discover the top 4 movies at any moment. That is not all!! Also you can search about any movie and know more information about it.</p>
+      </div>
       <SearchBar />
+      <h3>Top 4 Movies this moment</h3>
       <Movies />
     </div>
   );
