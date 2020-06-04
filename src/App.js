dbeaff52188ef8ffcf0a6f1686e5d41edfe8d267
Hanan795 <hawawdeh95@gmail.com>
2020-06-04 11:51:54 +0300

    create search bar and display search process result #8 #5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import SearchBar from './Components/SearchBar/SearchBar'
 
 function App() {
   return (
     <div className="App">
-      
+      <SearchBar />
     </div>
   );
 }
